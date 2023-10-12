6ef4907362ffc44566e3db74154a5c0010a36fa2
shanonline99 <dilshanthisaru1999@gmail.com>
2023-10-12 11:38:46 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+console.log("hey");
+
+var mysql = require('mysql2');
+const express = require('express');
+const app = express();
+
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "crud" 
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.listen(8081, () => {
+    console.log("listening");
+})
+
+app.get("/", (req,res) => {
+    const sql = "SELECT * FROM student";
+    con.query(sql, (err, data) => {
+        if(err) return res.json("Error");
+        return res.json(data);
+    })
+}) 
---
+++ frontend/src/components/Student.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Student() {
+  return (
+    <div>
+        Student
+
+    </div>
+  )
+}
+
+export default Student
